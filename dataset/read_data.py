ad19f8cf4f4288b9b30e7cbea6c82381606fe7d0
Mango <68170909+theawesomemango12@users.noreply.github.com>
2020-11-27 21:01:45 -0500

    Add files via upload

--- dataset/read_data.py
+++ dataset/read_data.py
@@ -1,8 +1,9 @@
-from pprint import pprint
 import csv
 from dataclasses import dataclass
 import plotly.express as px
-from typing import List
+from typing import List, Dict
+
+from scipy.sparse import data
 
 
 @dataclass
@@ -25,20 +26,68 @@ class GreenhouseGas:
     co2: float
     ch4: float
     n2o: float
-    hfc: float
-    pfc: float
-    sf6: float
-    nf3: float
+    # hfc: float
+    # pfc: float
+    # sf6: float
+    # nf3: float
     total: float
 
+    # def __init__(self, year, region, co2, ch4, n2o, total) -> None:
+    #     self.year = year
+    #     self.region = region
+    #     self.co2 = co2
+    #     self.ch4 = ch4
+    #     self.n2o = n2o
+    #     self.total = total
 
-def read_ghg_data(filename: str, last_row: int) -> List[GreenhouseGas]:
+
+
+class Bird:
+    """ An class representing a the data of a bird species """
+    data: dict
+
+    def __init__(self, bird_data: dict) -> None:
+        self.data = bird_data
+
+    def trim_data(self, start: int, end: int) -> None:
+        """ Create a new dict that contains all bird data from the year <start> to
+            the year <end> and reassign self.data to it.
+
+            Note: After the data is trimmed it cannot be reverted or untrimmed.
+
+            Preconditions:
+             - start < end
+             - 1970 <= end <= 2016
+             - 1970 <= start <= 2016
+
+        >>> bird = Bird({2000: 1.0, 2001: 2.0, 2003: 3.0})
+        >>> bird.trim_data(2000, 2001)
+        >>> bird.data
+        {2000: 1.0, 2001: 2.0}
+        """
+        self.data = {year: self.data[year] for year in range(start, end + 1)}
+
+    def find_first_point(self) -> int:
+        """ Return the first year where the data is not 'n/a' """
+        for year in self.data:
+            if self.data[year] != 'n/a':
+                return year
+
+    def data_to_list(self) -> float:
+        """ Return a list containing all the datapoints in data ordered by year
+        >>> bird = Bird({2000: 1.0, 2001: 2.0, 2003: 3.0})
+        >>> bird.data_to_list()
+        [1.0, 2.0, 3.0]
+        """
+        return [self.data[year] for year in self.data]
+
+def read_ghg_data(last_row: int) -> List[GreenhouseGas]:
     """ Return a list of GreenhouseGas instances, where each instance in
         the list represents a row in the data set.
 
         The function will read last_row number of rows
     """
-    with open(filename) as csvfile:
+    with open('GHG.csv') as csvfile:
         reader = csv.reader(csvfile)
         ghg_data = []
 
@@ -53,66 +102,56 @@ def read_ghg_data(filename: str, last_row: int) -> List[GreenhouseGas]:
                                           co2=float(row[5]),
                                           ch4=float(row[6]),
                                           n2o=float(row[8]),
-                                          hfc=float(row[10]),
-                                          pfc=float(row[11]),
-                                          sf6=float(row[12]),
-                                          nf3=float(row[13]),
+                                        #   hfc=float(row[10]),
+                                        #   pfc=float(row[11]),
+                                        #   sf6=float(row[12]),
+                                        #   nf3=float(row[13]),
                                           total=float(row[14])))
 
     return ghg_data
 
 
-def read_sea_level_data(filename) -> List[List]:
-    """ Return a list containing each row in the dataset as a
-        list.
+def read_bird_data() -> dict:
+        with open('bird_data.csv') as csvfile:
+            reader = csv.reader(csvfile)
+            # skips headers
+            for _ in range(3):
+                next(reader)
+
+            bird_data = {}
+
+            for _ in range(47):
+                row = next(reader)
+                bird_data[int(row[0])] = [row[1],
+                                          row[2],
+                                          row[3],
+                                          row[4],
+                                          row[5],
+                                          row[6],
+                                          row[7],
+                                          row[8],
+                                          row[9]]
+        return bird_data
+
+def filter_bird_data(bird_data: dict, column: int) -> dict:
+    """ Return a dictionary mapping the years in bird_data to a
+        specific column in the data
+
+        Preconditions:
+         - 0 <= column <= 8
+
+    >>> filter_bird_data({2002: ['0', '1', '2', '3', '4', '5'],\
+                          2003: ['0', '1', '2', '3', '4', '5'] }, 0)
+    {2002: 0, 2003: 0}
     """
-    # sea level data processing
-    with open(filename) as csvfile:
-        reader = csv.reader(csvfile)
-        sea_level = []
-        for row in reader:
-            sea_level.append(row)
-
-    return sea_level[6:]  # trims the data, first 6 rows are headers and text
-
-
-def sea_level_to_dict(sea_level: list) -> dict:
-    """ Return the mapping of year to average sea level during
-        that year
-    """
-    sea_mapping = {}  # mapping of year to average sea level
-    for row in sea_level:
-        # turns the year from a string into an int
-        # consequently floors the year, as wanted
-        year = int(float(row[0]))
-
-        # average sea level for given datapoint
-        average = average_sea_level(row)
-
-        # appends the average sea level for a certain part of the year to the dict
-        if year in sea_mapping:
-            sea_mapping[year].append(average)
+    filtered_dict = {}
+    for year in bird_data:
+        column_data = bird_data[year][column]
+        if column_data != 'n/a':
+            filtered_dict[year] = float(column_data)
         else:
-            sea_mapping[year] = [average]
+            filtered_dict[year] = 'n/a'
 
-    return average_per_year(sea_mapping)
+    return filtered_dict
 
-
-def average_sea_level(row: list) -> float:
-    """ Return the average sea level for the given row """
-    total_sea_level = sum([float(num) for num in row[1:] if num != ''])
-    data_sources = (4 - row[1:].count(''))
-
-    return total_sea_level / data_sources
-
-
-def average_per_year(sea_mapping: dict) -> dict:
-    """ Return the average sea level per year """
-    sealevel_per_year = {}
-    for year in sea_mapping:
-        total_per_year = sum(sea_mapping[year])
-        datapoints_per_year = len(sea_mapping[year])
-
-        sealevel_per_year[year] = total_per_year / datapoints_per_year
-
-    return sealevel_per_year
+# def filter_ghg_data(ghg_data: List[GreenhouseGas]) -> List[float]:
--- dataset/regression.py
+++ dataset/regression.py
@@ -4,48 +4,64 @@ import plotly.graph_objects as go
 import plotly.express as px
 from sklearn.linear_model import LinearRegression
 
+# helper function
+def lists_to_array(x: list, y: list) -> tuple:
+    """ Return the x and y as a tuple of numpy arrays and
+        reshape the x array to (-1, 1), so that it is one dimensional
+    """
+    x_array = np.array(x).reshape(-1, 1)
+    y_array = np.array(y)
 
-def build_model(gas_data: List[float],  sea_data: List[float]) -> tuple:
-    arrays = lists_to_array(gas_data, sea_data)
+    return (x_array, y_array)
+
+def build_model(gas_data: List[float],  bird_data: List[float]) -> LinearRegression():
+    """Return the Linear Regression model from the given data"""
+    arrays = lists_to_array(gas_data, bird_data)
     x_data = arrays[0]
     y_data = arrays[1]
     model = LinearRegression().fit(x_data, y_data)
 
     return model
 
+class RegressionModel:
+    # Private instance attribute
+    _model: LinearRegression
 
-def lists_to_array(x: list, y: list) -> tuple:
-    x_array = np.array(x).reshape(-1, 1)
-    y_array = np.array(y)
+    def __init__(self, ghg_data: List[float], bird_data: List[float]) -> None:
+        self._model = build_model(ghg_data, bird_data)
 
-    return (x_array, y_array)
+    def predict_data(self, x: float) -> float:
+        m = float(self._model.coef_)
+        b = float(self._model.intercept_)
+        return m * x + b
 
+    def plot_data(self, x_values: List[float], y_values: List[float], title) -> None:
+        x_range = [min(x_values), max(x_values)]
+        y_range = [self.predict_data(x_range[0]),
+                   self.predict_data(x_range[1])]
 
-def predict_data(x: float , model: LinearRegression) -> float:
-    m = float(model.coef_)
-    b = float(model.intercept_)
-    return m * x + b
+        fig = px.scatter(x=x_values,
+                         y=y_values,
+                         title=title,
+                         labels = {'x': "Greenhouse Gas Emissions (kt)",
+                                   'y': "Percentage of Change in Bird Population from 1970"})
 
+        fig.add_traces(go.Scatter(x=x_range, y=y_range, name="Regression Line"))
+        fig.show()
 
-def plot_data(x_values: List[float], y_values: List[float], model: LinearRegression, title: str) -> None:
-    x_range = [min(x_values), max(x_values)]
-    y_range = [predict_data(x_range[0], model), predict_data(x_range[1], model)]
-
-    fig = px.scatter(x=x_values,
-                     y=y_values,
-                     title=title,
-                     labels = {'x': "Greenhouse Gas Emissions (kt)", 'y': "Mean Sea Level (mm)"})
-    fig.add_traces(go.Scatter(x=x_range, y=y_range, name="Regression Line"))
-    fig.show()
-
-
-ghg_data = read_ghg_data('GHG.csv', 30)
-sea_data = read_sea_level_data('sea_levels.csv')
-sea_dict = sea_level_to_dict(sea_data)
+ghg_data = read_ghg_data(29)
+co2 = [data.co2 for data in ghg_data if 1990 <= data.year <= 2016]
 
 
-total = [datapoint.total for datapoint in ghg_data if datapoint.year in range(1992, 2019)]
-sea_level =[sea_dict[year] for year in range(1992, 2019)]
+bird_data = read_bird_data()
+waterfowl_data = filter_bird_data(bird_data, 8)
+waterfowl = Bird(waterfowl_data)
+waterfowl.trim_data(1990, 2016)
+waterfowl_data = waterfowl.data_to_list()
 
-model = build_model(total, sea_level)
-plot_data(total, sea_level, model, 'Data')
+
+model = RegressionModel(co2, waterfowl_data)
+model.plot_data(co2, waterfowl_data, 'Test')
+
+
+
