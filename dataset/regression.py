00cbc4208a8a5efb20d9f9b0f028e06f29460762
Mango <68170909+theawesomemango12@users.noreply.github.com>
2020-11-30 14:34:56 -0500

    Add files via upload

--- dataset/regression.py
+++ dataset/regression.py
@@ -2,7 +2,8 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 from sklearn.linear_model import LinearRegression
-from typing import List
+from typing import List, Tuple
+from read_data import *
 
 class RegressionModel:
     """ A class representing the linear regression model of the given data """
@@ -17,7 +18,7 @@ class RegressionModel:
         self._ghg_data = ghg_data
         self._model = self._build_model()
 
-    def _build_model(self) -> LinearRegression():
+    def _build_model(self) -> LinearRegression:
         """ Return the Linear Regression model from the given data """
         arrays = self._lists_to_array(self._ghg_data, self._bird_data)
         x_data = arrays[0]
@@ -25,7 +26,7 @@ class RegressionModel:
         model = LinearRegression().fit(x_data, y_data)
         return model
 
-    def _lists_to_array(self, x: list, y: list) -> tuple:
+    def _lists_to_array(self, x: list, y: list) -> Tuple[np.array, np.array]:
         """ Return the x and y as a tuple of numpy arrays and
         reshape the x array to (-1, 1), so that it is one dimensional
         """
@@ -54,9 +55,12 @@ class RegressionModel:
         return (y - b) / m
 
     def plot_data(self, title: str, x_label: str, y_label: str) -> None:
+        """Plot the given data with a line of best fit generated from the
+        regression model
+        """
         x_range = [min(self._ghg_data), max(self._ghg_data)]
-        y_range = [self.predict_data(x_range[0]),
-                   self.predict_data(x_range[1])]
+        y_range = [self.predict_y(x_range[0]),
+                   self.predict_y(x_range[1])]
 
         fig = px.scatter(x=self._ghg_data,
                          y=self._bird_data,
@@ -69,20 +73,44 @@ class RegressionModel:
 
 
 # class MultipleRegression:
-
 #     _model: LinearRegression
+#     _ghg_data: List[List[float]]
+#     _bird_data: List[float]
 
+#     def __init__(self, ghg_data: List[List[float]], bird_data: List[float]) -> None:
+#         self._ghg_data = ghg_data
+#         self._bird_data = bird_data
+#         self._model = self._build_model()
 
+#     def _build_model(self) -> LinearRegression:
+#         x_values = np.array(self._ghg_data).reshape(1, -1)
+#         y_values = np.array(self._bird_data)
+#         model = LinearRegression().fit(x_values, y_values)
+#         return model
 
+#     def plot_data(self, title: str, x_label: str, y_label: str) -> None:
+#         """Plot the given data with a line of best fit generated from the
+#         regression model
+#         """
+#         x_range = [min(self._ghg_data), max(self._ghg_data)]
+#         y_range = [self.predict_data(x_range[0]),
+#                    self.predict_data(x_range[1])]
 
+#         fig = px.scatter(x=self._ghg_data,
+#                          y=self._bird_data,
+#                          title=title,
+#                          labels = {'x': x_label,
+#                                    'y': y_label})
 
+#         fig.add_traces(go.Scatter(x=x_range, y=y_range, name="Regression Line"))
+#         fig.show()
 
 #########################################################################################################################
 # Example Usage
 #########################################################################################################################
 
-# ghg_data = read_ghg_data(50)  # reads the data
-# keys = ghg_data.keys()  # gets all the possible keys
+# ghg_data = read_ghg_data(100)  # reads the data
+# # keys = ghg_data.keys()  # gets all the possible keys
 # alberta = Province(ghg_data['Alberta'])  # creates a Province object
 
 # bird_data = read_bird_data()  # reads the bird data
@@ -90,6 +118,8 @@ class RegressionModel:
 # bird = Bird(bird_data)  # creates an Bird Object
 # bird.trim_data(1990, 2016)  # trims the bird data to match the length of the GHG data
 
-# model = RegressionModel(alberta.total, bird.list_data)  # creates the linear regression model
+# # model = RegressionModel(alberta.total, bird.list_data)  # creates the linear regression model
+# # model.predict(69)  # predicts the index of change of bird species for 69 kt of emission
+# x = [alberta.co2, alberta.ch4, alberta.sf6]
+# model = MultipleRegression(x, [bird.list_data])
 # model.plot_data('Test', 'x', 'y')  # plots the data
-# model.predict(69)  # predicts the index of change of bird species for 69 kt of emission
