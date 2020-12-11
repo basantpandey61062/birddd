1ff8574b66758fda89a75eeaed77246c01fe5bb5
Mango <68170909+theawesomemango12@users.noreply.github.com>
2020-12-10 21:18:26 -0500

    Add files via upload

--- regression.py
+++ regression.py
@@ -7,18 +7,29 @@ import pandas
 import plotly.graph_objects as go
 import plotly.express as px
 from sklearn.linear_model import LinearRegression
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 
 class RegressionModel:
     """ A class representing the linear regression model of the given data """
 
     # Private instance attributes
+    #   -_model: the linear regression model
+    #   -_ghg_data: a list of floats representing the ghg emissions of a region
+    #   -_bird_data: a list of floats representing the percentage change of a
+    #    species of birds since 1970
     _model: LinearRegression
     _ghg_data: List[float]
     _bird_data: List[float]
 
     def __init__(self, ghg_data: List[float], bird_data: List[float]) -> None:
+        """ Initialize the RegressionModel
+
+        Preconditins:
+            - ghg_data is a list of floats representing the greenhouse gas emissions
+              of a region
+            - bird_data is a list of percentage changes for a specific bird species
+        """
         self._bird_data = bird_data
         self._ghg_data = ghg_data
         self._model = self._build_model()
@@ -62,6 +73,7 @@ class RegressionModel:
         x_range = [min(self._ghg_data), max(self._ghg_data)]
         y_range = [self.predict_y(x_range[0]),
                    self.predict_y(x_range[1])]
+
         fig = px.scatter(x=self._ghg_data,
                          y=self._bird_data,
                          title=f'{title} {" " * 10} R^2 = {self.get_r_squared()}',
@@ -71,6 +83,55 @@ class RegressionModel:
         fig.add_traces(go.Scatter(x=x_range, y=y_range, name="Regression Line"))
         fig.show()
 
+
+
+
+class MultipleRegression:
+    """Class Representing the multiple regression model for the given data"""
+    # Private Attributes
+    #   - _model: the multiple regression model
+    #   -_x_variables: a pandas Dataframe representing the given ghg data
+    _model: LinearRegression
+    _x_variables: pandas.DataFrame
+
+    def __init__(self, x_variables: Dict[str, List[float]], y_values: List[float]) -> None:
+        """Initialize the _model attribute
+        Preconditions:
+            - x_variables is a dictionary mapping names of GHGs to a list of floats
+              representing the annual emissions of a region
+            - y_values is a list of floats representing the index change for a species of birds
+        """
+        self._x_variables = pandas.DataFrame(x_variables)
+        self._model = LinearRegression().fit(pandas.DataFrame(x_variables),
+                                             y_values)
+
+    def predict_value(self,
+                      co2: float,
+                      ch4: float,
+                      n2o: float,
+                      hfc: float,
+                      pfc: float,
+                      sf6: float,
+                      nf3: float
+                    ) -> float:
+        """ Return the estimated percentage change since 1970 of birds for the given
+        greenhouse gas values base off of the LinearRegression Model.
+        """
+        return float(self._model.predict(co2, ch4, n2o, hfc, pfc, sf6, nf3))
+
+    def plot_coef(self, bird_name: str) -> None:
+        """Plot the coefficients with a bar graph"""
+        columns =[column.upper() for column in self._x_variables.columns]
+        rows = [float(row) for row in self._model.coef_]
+
+        fig = px.bar(x=columns,
+                     y=rows,
+                     labels={'x': 'Greenhouse Gasses', 'y': 'Coefficient'},
+                     title=f'The weight of each Greenhouse gas on the percentage change of {bird_name} since 1970'
+
+        )
+
+        fig.show()
 
 # Helper Function
 def _lists_to_array(x: list, y: list) -> Tuple[np.array, np.array]:
@@ -86,15 +147,16 @@ def _lists_to_array(x: list, y: list) -> Tuple[np.array, np.array]:
 ########################################################################################################################
 # Example Usage
 ########################################################################################################################
+from read_data import *
 
-# ghg_data = read_ghg_data(398)  # reads the data
+ghg_data = read_ghg_data(398)  # reads the data
 # # keys = ghg_data.keys()  # gets all the possible keys
 # alberta = Province(ghg_data['Alberta'])  # creates a Province object
-# canada = Province(ghg_data['Canada'])
-# bird_data = read_bird_data()  # reads the bird data
-# bird_data = filter_bird_data(bird_data, 8)  # filters the bird data so that only column 8 remains
-# bird = Bird(bird_data)  # creates an Bird Object
-# bird.trim_data(1990, 2016)  # trims the bird data to match the length of the GHG data
+canada = Region(ghg_data['Canada'])
+bird_data = read_bird_data()  # reads the bird data
+bird_data = filter_bird_data(bird_data, 8)  # filters the bird data so that only column 8 remains
+bird = Bird(bird_data)  # creates an Bird Object
+bird.adjust_data(1990, 2016)  # trims the bird data to match the length of the GHG data
 # model = RegressionModel(canada.total, bird.list_data)  # creates the linear regression model
 # # model.predict_y(69)  # predicts the index of change of bird species for 69 kt of emission
 # model.plot_data('Test', 'x', 'y')  # plots the data
@@ -112,17 +174,15 @@ def _lists_to_array(x: list, y: list) -> Tuple[np.array, np.array]:
 #                         'pfc': canada.pfc,
 #                         'sf6': canada.sf6,
 #                         'nf3': canada.nf3})
+canada.initialize_lists(1990, 2016)
+x = {'co2': canada.co2,
+    'ch4': canada.ch4,
+    'n2o': canada.n2o,
+    'hfc': canada.hfc,
+    'pfc': canada.pfc,
+    'sf6': canada.sf6,
+    'nf3': canada.nf3}
 
-# Builds Multiple Regression model. You can just copy and past this part and change some variables
-# You need the df tho.
-# model = LinearRegression().fit(df[['co2', 'ch4', 'n2o', 'hfc', 'pfc', 'sf6', 'nf3']],
-#                                                    bird.list_data)
-
-# You can use this to output the equation of the line to the user maybe
-# model.coef_  # gets coefficients for the equation representing the model
-
-# Predicting the data
-# model.predict(co2, ch4, n2o, hfc, pfc, sf6, nf3)  # predicts the data takes SEVEN parameters
-# Note: the function above doesn't output a float.
-# Instead you should wrap the float around it like the following
-# float(model.predict(co2, ch4, n2o, hfc, pfc, sf6, nf3))
+model = MultipleRegression(x, bird.list_data)
+print(model._model.coef_)
+model.plot_coef('Title')
