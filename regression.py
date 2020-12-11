3b197a23be00ee22ebcdc39f08b1e6d34b11d0dd
Mango <68170909+theawesomemango12@users.noreply.github.com>
2020-12-10 22:25:32 -0500

    Add files via upload

--- regression.py
+++ regression.py
@@ -125,7 +125,7 @@ class MultipleRegression:
         Preconditions:
             - all(value >= 0 for value in {co2, ch4, n2o, hfc, pfc, sf6, nf3})
         """
-        return float(self._model.predict(co2, ch4, n2o, hfc, pfc, sf6, nf3))
+        return float(self._model.predict([[co2, ch4, n2o, hfc, pfc, sf6, nf3]]))
 
     def _get_coef(self) -> Dict[str, float]:
         """Return a dictionary mapping the name of a greenhouse gas to
@@ -156,16 +156,16 @@ def _lists_to_array(x: list, y: list) -> Tuple[np.array, np.array]:
 ########################################################################################################################
 # Example Usage
 ########################################################################################################################
-# from read_data import *
+from read_data import *
 
-# ghg_data = read_ghg_data(398)  # reads the data
+ghg_data = read_ghg_data(398)  # reads the data
 # # # keys = ghg_data.keys()  # gets all the possible keys
 # # alberta = Province(ghg_data['Alberta'])  # creates a Province object
-# canada = Region(ghg_data['Alberta'])
-# bird_data = read_bird_data()  # reads the bird data
-# bird_data = filter_bird_data(bird_data, 8)  # filters the bird data so that only column 8 remains
-# bird = Bird(bird_data)  # creates an Bird Object
-# bird.adjust_data(1990, 2016)  # trims the bird data to match the length of the GHG data
+canada = Region(ghg_data['Alberta'])
+bird_data = read_bird_data()  # reads the bird data
+bird_data = filter_bird_data(bird_data, 8)  # filters the bird data so that only column 8 remains
+bird = Bird(bird_data)  # creates an Bird Object
+bird.adjust_data(1990, 2016)  # trims the bird data to match the length of the GHG data
 # model = RegressionModel(canada.total, bird.list_data)  # creates the linear regression model
 # # model.predict_y(69)  # predicts the index of change of bird species for 69 kt of emission
 # model.plot_data('Test', 'x', 'y')  # plots the data
@@ -183,15 +183,14 @@ def _lists_to_array(x: list, y: list) -> Tuple[np.array, np.array]:
 #                         'pfc': canada.pfc,
 #                         'sf6': canada.sf6,
 #                         'nf3': canada.nf3})
-# canada.initialize_lists(1990, 2016)
-# x = {'co2': canada.co2,
-#     'ch4': canada.ch4,
-#     'n2o': canada.n2o,
-#     'hfc': canada.hfc,
-#     'pfc': canada.pfc,
-#     'sf6': canada.sf6,
-#     'nf3': canada.nf3}
+canada.initialize_lists(1990, 2016)
+x = {'co2': canada.co2,
+    'ch4': canada.ch4,
+    'n2o': canada.n2o,
+    'hfc': canada.hfc,
+    'pfc': canada.pfc,
+    'sf6': canada.sf6,
+    'nf3': canada.nf3}
 
-# model = MultipleRegression(x, bird.list_data)
-# print(model._model.coef_)
-# model.plot()
+model = MultipleRegression(x, bird.list_data)
+print(model._model.coef_)
